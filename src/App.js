867c90276ec41a1bc4a3e20b89dcf6c5f83b903c
Harsh <agr.harsh.2000@gmail.com>
2023-05-21 14:56:35 +0530

    first page completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="FirstPage">
+      <div className="PageHeader">
+        <h2 className="PageTitle">FORMZ</h2>
+        <button className="LoginButton">Login</button>
+      </div>
+      <div className="FirstPageContent">
+        <p className="FirstPageTitleContext">
+          We make Forms, that’s it oh, but Better!
+        </p>
+        <p className="FirstPageTitleDescription">
+          At the end of the day developers are asked to make the business forms,
+          hence it’s time to make them developer friendly
+        </p>
+        <button className="GetStartedButton">GetStarted</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
